9cde15e655ecdec595ff9698260756a6d20f1291
Anca Brezeanu <brezeanuanca21@stud.ase.ro>
2025-05-23 19:43:48 +0300

    'rezolvareafisarepostman'

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ utils/constants.js
@@ -0,0 +1,8 @@
+// /utils/constants.js
+
+export const recordDefaultValues = {
+    _id: "",
+    title: "",
+    date: "",
+    completed:""
+};
